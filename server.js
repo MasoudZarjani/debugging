8f119a28edb56c41d048c6769886fba05e9f6c1c
masoud zarjani <masoud.mail.info@gmail.com>
2021-04-13 14:21:26 +0430

    last

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import router from "./routes";
 import { mongodb } from "./config/db";
 
